e5c4997bd29749e1005606f75e612805df6ede6b
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-02-21 14:44:33 +0100

    Edit

--- install.sh
+++ install.sh
@@ -201,7 +201,7 @@ install_rpi_specific() {
     
     # Configure Bluetooth
     echo "Installing BT connectivity..."
-    curl -L https://raw.githubusercontent.com/oulianov/Rpi-SetWiFi-viaBluetooth/refs/heads/main/btwifisetInstall.sh | bash -s -- -y
+    curl -L https://raw.githubusercontent.com/nksan/Rpi-SetWiFi-viaBluetooth/refs/heads/main/btwifisetInstall.sh | bash 
 }
 
 install_darwin_specific() {
